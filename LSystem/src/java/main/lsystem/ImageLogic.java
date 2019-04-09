2780b8a0f9c8dec9763e592f90965ea0304e77f6
halmelachine <outrolledeye@gmail.com>
2019-04-09 19:49:54 +0300

    proggis ihan vitun rikki, samoin oma terveys. Meen nyt nukkumaan, katotaan ehdinkö takas

---
+++ LSystem/src/java/main/lsystem/ImageLogic.java
@@ -0,0 +1,18 @@
+package lsystem;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.HashMap;
+
+public class ImageLogic {
+    private Deque<Vector> drawStack;
+    private HashMap<String, String> drawingRules;
+
+
+
+    public HashMap<String, String> ruleCreator(ArrayList<String> alphabet) {
+        
+    }
+
+
+}
